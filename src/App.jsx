4da84a0586938f3a78d8ca733429090d675d21ce
Mezz <mezzanine510@gmail.com>
2024-12-21 15:43:31 -0800

    component composition example

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,6 @@
-import Header from './components/Header.jsx';
+import Header from './components/Header/Header.jsx';
 import CoreConcept from './components/CoreConcept.jsx';
+import TabButton from "./components/TabButton";
 import { CORE_CONCEPTS } from './data.js';
 
 
@@ -20,6 +21,15 @@ function App() {
                         <CoreConcept {...CORE_CONCEPTS[2]} />
                         <CoreConcept {...CORE_CONCEPTS[3]} />
                     </ul>
+                </section>
+                <section id="examples">
+                    <h2>Examples</h2>
+                    <menu>
+                        <TabButton>Components</TabButton>
+                        <TabButton>JSX</TabButton>
+                        <TabButton>Props</TabButton>
+                        <TabButton>State</TabButton>
+                    </menu>
                 </section>
             </main>
         </div>
