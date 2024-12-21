403547c4aa92d07c0c47494cc600e532e8a55762
Mezz <mezzanine510@gmail.com>
2024-12-21 14:47:22 -0800

    props practice + destructuring

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,6 @@
 import reactImg from './assets/react-core-concepts.png';
+import componentsImg from './assets/components.png';
+import { CORE_CONCEPTS } from './data.js';
 
 
 const reactDescriptions = ['Fundamental', 'Crucial', 'Core'];
@@ -12,7 +14,7 @@ function Header() {
 
     return (
         <header>
-            <img src={reactImg} alt="Stylized atom" />
+            <img src={reactImg} alt="Stylized atom"/>
             <h1>React Essentials</h1>
             <p>
                 {description} Fundamental React concepts you will need for almost any app you are going to build!
@@ -21,12 +23,34 @@ function Header() {
     );
 }
 
+function CoreConcept({image, title, description}) {
+    return (
+        <li>
+            <img src={image} alt={title} />
+            <h3>{title}</h3>
+            <p>{description}</p>
+        </li>
+    );
+}
+
 function App() {
     return (
         <div>
-            <Header />
+            <Header/>
             <main>
-                <h2>Time to get started!</h2>
+                <section id="core-concepts">
+                    <h2>Core Concepts</h2>
+                    <ul>
+                        <CoreConcept
+                            title={CORE_CONCEPTS[0].title}
+                            description={CORE_CONCEPTS[0].description}
+                            image={CORE_CONCEPTS[0].image}
+                        />
+                        <CoreConcept {...CORE_CONCEPTS[1]} />
+                        <CoreConcept {...CORE_CONCEPTS[2]} />
+                        <CoreConcept {...CORE_CONCEPTS[3]} />
+                    </ul>
+                </section>
             </main>
         </div>
     );
