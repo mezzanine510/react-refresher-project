e930976b78ba2a5372bdfdd51e454f4806b4c881
Mezz <mezzanine510@gmail.com>
2024-12-21 17:47:01 -0800

    conditional rendering

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,35 @@
+import {useState} from 'react';
 import Header from './components/Header/Header.jsx';
 import CoreConcept from './components/CoreConcept.jsx';
-import TabButton from "./components/TabButton";
-import { CORE_CONCEPTS } from './data.js';
+import TabButton from './components/TabButton';
+import {CORE_CONCEPTS} from './data.js';
+import {EXAMPLES} from './data.js';
 
 
 function App() {
+    // must be called on the top level of the component function
+    const [selectedTopic, setSelectedTopic] = useState();
+
+    function handleSelect(selectedButton) {
+        setSelectedTopic(selectedButton);
+        // console.log(selectedButton);
+    }
+
+    console.log("APP COMPONENT EXECUTING");
+
+    let tabContent = <p>Select a topic to see an example</p>;
+    if (selectedTopic) {
+        tabContent = (
+            <div id="tab-content">
+                <h3>{EXAMPLES[selectedTopic].title}</h3>
+                <p>{EXAMPLES[selectedTopic].description}</p>
+                <pre>
+                    <code>{EXAMPLES[selectedTopic].code}</code>
+                </pre>
+            </div>
+        );
+    }
+
     return (
         <div>
             <Header/>
@@ -25,11 +50,12 @@ function App() {
                 <section id="examples">
                     <h2>Examples</h2>
                     <menu>
-                        <TabButton>Components</TabButton>
-                        <TabButton>JSX</TabButton>
-                        <TabButton>Props</TabButton>
-                        <TabButton>State</TabButton>
+                        <TabButton onSelect={() => handleSelect('components')}>Components</TabButton>
+                        <TabButton onSelect={() => handleSelect('jsx')}>JSX</TabButton>
+                        <TabButton onSelect={() => handleSelect('props')}>Props</TabButton>
+                        <TabButton onSelect={() => handleSelect('state')}>State</TabButton>
                     </menu>
+                    {tabContent}
                 </section>
             </main>
         </div>
--- src/components/TabButton.jsx
+++ src/components/TabButton.jsx
@@ -1,10 +1,8 @@
-export default function TabButton({ children }) {
-    function handleClick() {
-        console.log('Hello wurld!');
-    }
+export default function TabButton({ children, onSelect }) {
+    console.log("TABBUTTON COMPONENT EXECUTING");
     return (
         <li>
-            <button onClick={handleClick}>{children}</button>
+            <button onClick={onSelect}>{children}</button>
         </li>
     );
 }
